2e93d6a75d2ec6df81f0aa7b9e8755d408578b38
Atul Pandey <35248621+atulpandey34@users.noreply.github.com>
2021-10-03 09:22:13 +0530

    more changes

--- Angular_WebAPI/UI/src/app/containers/default-layout/default-layout.component.ts
+++ Angular_WebAPI/UI/src/app/containers/default-layout/default-layout.component.ts
@@ -1,5 +1,7 @@
 import {Component} from '@angular/core';
 import { navItems } from '../../_nav';
+import { AuthenticationService } from '../../services/authentication.service'
+import { ActivatedRoute, Router } from '@angular/router'
 
 @Component({
   selector: 'app-dashboard',
@@ -9,7 +11,17 @@ export class DefaultLayoutComponent {
   public sidebarMinimized = false;
   public navItems = navItems;
 
+  constructor(private authservice: AuthenticationService, private route: ActivatedRoute, private router: Router) {
+
+  }
+
   toggleMinimize(e) {
     this.sidebarMinimized = e;
+  }
+
+  logout() {
+    debugger;
+    this.authservice.logout();
+    this.router.navigate(['/login']);
   }
 }
